90ff6f0f5433b13ce6358afc5bef4ee955984042
Muhammad Iqbal Pradipta <mhdiqbalpradipta@gmail.com>
2025-02-06 13:54:03 +0700

    perbaikan palang untuk terbuka

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,11 @@ const int pinAktif = 3;
 const int pinMobil = 6;
 const int pinMotor = 5;
 
-int delayWaktuAktif = 1000;
-int lamaAktif = 5000;
+int delayWaktuAktif = 50;
+int lamaAktif = 1000;
 int delayWaktuKendaraan = 1000;
 int lamaAktifKendaraan = 5000;
+int kendaraanAktif = 0;
 
 void setup() {
   // put your setup code here, to run once:
@@ -23,29 +24,51 @@ Serial.begin(9600); //BAUD RATE ke golang
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(pinTiket) == HIGH)
+  if ((digitalRead(pinTiket) == HIGH) && (kendaraanAktif == 1)) 
   {
-    int nilaiTiket = 1;
-    //Serial.print("Tiket,");
-    Serial.print(nilaiTiket);
-    delay(delayWaktuAktif);
+    Serial.print("ini tiket");
+    Serial.print(kendaraanAktif);
+    // delay(delayWaktuAktif);
     digitalWrite(pinAktif, HIGH);
-    delay(lamaAktif);
-    digitalWrite(pinAktif, LOW);
+    kendaraanAktif = 0;
+    delay(delayWaktuAktif);
+    // digitalWrite(pinAktif, LOW); 
+    delay(lamaAktifKendaraan);
+
   }
 
   if (digitalRead(pinMobil) == HIGH){
     int nilaiMobil = 2;
+    Serial.print("ini nilai kosong");
+
     //Serial.print("Mobil,");
     Serial.print(nilaiMobil);
-    delay(delayWaktuKendaraan);
+    // digitalWrite(pinMobil, HIGH);
+    // digitalWrite(pinAktif, HIGH);
+    kendaraanAktif = 1;
+    // digitalWrite(pinAktif, HIGH);
+    // delay(delayWaktuKendaraan);
+    delay(delayWaktuAktif);
+    // digitalWrite(pinAktif, LOW);
     delay(lamaAktifKendaraan);
+    Serial.print("ini nilai kosong II");
+
   }
-    if (digitalRead(pinMotor) == HIGH){
+  if (digitalRead(pinMotor) == HIGH){
+    // Serial.print("ini motor");
+
     int nilaiMotor = 3;
     //Serial.print("Motor,");
-    Serial.print(nilaiMotor);
-    delay(delayWaktuKendaraan);
-    delay(lamaAktifKendaraan);
-  }
+    // Serial.print(nilaiMotor);
+    kendaraanAktif = 1;
+    // digitalWrite(pinAktif, HIGH);
+    // digitalWrite(pinMotor, HIGH);
+    // delay(delayWaktuKendaraan);
+    // delay(lamaAktifKendaraan);
+  } 
+  // Serial.print("ini kendaraan aktif");
+  // Serial.println("ini kendaraan : " + String(kendaraanAktif));
+  digitalWrite(pinAktif, LOW); 
+  // kendaraanAktif = 0;
+  
 }
